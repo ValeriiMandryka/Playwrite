8183bfb85b9e71f54884a3b5327fb6aafce7bd3a
Valerii <mandryka.valerii@gmail.com>
2022-10-11 12:07:06 +0300

    modified:   pageObjects/searchPage.js

---
+++ pageObjects/searchPage.js
@@ -0,0 +1,10 @@
+class SearchPage extends GeneralPage {
+ 
+    constructor(page) {
+        super(page);
+        this.page = page;
+        }
+        //function openMainPage taken from generalPage
+    
+    }
+    module.exports = { SearchPage };
